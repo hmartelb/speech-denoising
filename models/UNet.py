9a1dcc65f3ce1b3f34cb40803d18e207c8f25a3f
spkgyk <spkgyk@gmail.com>
2021-05-18 01:44:03 +0100

    added model unet to models

---
+++ models/UNet.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+
+
+def double_conv(in_channels, out_channels, kernel_size, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding),
+        nn.ReLU(inplace=True),
+    )
+
+
+class UNet(nn.Module):
+    def __init__(
+        self,
+        n_channels=1,
+        n_class=2,
+        unet_depth=5,
+        unet_scale_factor=64,
+        double_conv_kernel_size=3,
+        double_conv_padding=1,
+        maxpool_kernel_size=2,
+        upsample_scale_factor=2,
+    ):
+        super().__init__()
+
+        self.depth = unet_depth
+
+        self.conv_down = nn.ModuleList(
+            [double_conv(n_channels, unet_scale_factor, double_conv_kernel_size, double_conv_padding)]
+            + [
+                double_conv(
+                    unet_scale_factor * (2 ** i),
+                    unet_scale_factor * (2 ** (i + 1)),
+                    double_conv_kernel_size,
+                    double_conv_padding,
+                )
+                for i in range(unet_depth)
+            ]
+        )
+
+        self.maxpool = nn.MaxPool2d(maxpool_kernel_size)
+        self.upsample = nn.Upsample(scale_factor=upsample_scale_factor, mode="bilinear", align_corners=True)
+
+        self.conv_up = nn.ModuleList(
+            [
+                double_conv(
+                    unet_scale_factor * (2 ** (unet_depth - i - 1)),
+                    unet_scale_factor * (2 ** (unet_depth - i - 2)),
+                    double_conv_kernel_size,
+                    double_conv_padding,
+                )
+                for i in range(unet_depth - 1)
+            ]
+            + [nn.Conv2d(unet_scale_factor, n_class, 1)]
+        )
+
+    def forward(self, x):
+        storage = []
+        last_item = self.depth - 1
+
+        for i in range(self.depth):
+            x = self.conv_down[i](x)
+            if i != last_item:
+                storage.append(x)
+                x = self.maxpool(x)
+
+        storage.reverse()
+
+        for i in range(self.depth):
+            if i != last_item:
+                x = self.upsample(x)
+                x = self.conv_up[i](x)
+                x += storage[i]
+            else:
+                x = self.conv_up[i](x)
+
+        return x
+
+
+# from torchsummary import summary
+# import os
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# model = UNet()
+# summary(model, (1, 256, 256))
+# x = torch.ones([20, 1, 256, 256])
+# y = model.forward(x)
+
+# print(y.shape)
--- models/UNet_hardcodeed.py
+++ models/UNet_hardcodeed.py
@@ -1,87 +1,80 @@
 import torch
 import torch.nn as nn
 
+
 def double_conv(in_channels, out_channels):
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, 3, padding=1),
         nn.ReLU(inplace=True),
         nn.Conv2d(out_channels, out_channels, 3, padding=1),
-        nn.ReLU(inplace=True)
-    )   
+        nn.ReLU(inplace=True),
+    )
 
 
 class UNet(nn.Module):
-
     def __init__(self, in_ch, n_class):
         super().__init__()
-                
+
         self.dconv_down1 = double_conv(in_ch, 64)
         self.dconv_down2 = double_conv(64, 128)
         self.dconv_down3 = double_conv(128, 256)
-        self.dconv_down4 = double_conv(256, 512) 
-        self.dconv_down5 = double_conv(512, 512)         
+        self.dconv_down4 = double_conv(256, 512)
+        self.dconv_down5 = double_conv(512, 512)
 
         self.maxpool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
-        
-        self.dconv_up4 = double_conv(512, 512)  
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+
+        self.dconv_up4 = double_conv(512, 512)
         self.dconv_up3 = double_conv(512, 256)
         self.dconv_up2 = double_conv(256, 128)
         self.dconv_up1 = double_conv(128, 64)
-        
+
         self.conv_last = nn.Conv2d(64, n_class, 1)
-        
-        
+
     def forward(self, x):
         conv1 = self.dconv_down1(x)
         x = self.maxpool(conv1)
 
         conv2 = self.dconv_down2(x)
         x = self.maxpool(conv2)
-        
+
         conv3 = self.dconv_down3(x)
-        x = self.maxpool(conv3)   
-        
+        x = self.maxpool(conv3)
+
         conv4 = self.dconv_down4(x)
         x = self.maxpool(conv4)
 
         x = self.dconv_down5(x)
-        
-        x = self.upsample(x)    
-        print(x.shape, conv4.shape)
-        x += conv4        
-        x = self.dconv_up4(x)     
-        
 
-        x = self.upsample(x)        
-        # x = torch.cat([x, conv3], dim=1)   
+        x = self.upsample(x)
+        x += conv4
+        x = self.dconv_up4(x)
+
+        x = self.upsample(x)
         x = self.dconv_up3(x)
-        print(x.shape, conv3.shape)    
         x += conv3
 
-        x = self.upsample(x)        
-        # x = torch.cat([x, conv2], dim=1)    
+        x = self.upsample(x)
         x = self.dconv_up2(x)
-        print(x.shape, conv2.shape)
-        x += conv2       
+        x += conv2
 
-        x = self.upsample(x)        
-        # x = torch.cat([x, conv1], dim=1)      
+        x = self.upsample(x)
         x = self.dconv_up1(x)
-        print(x.shape, conv1.shape)
-        x += conv1     
-        
+        x += conv1
+
         out = self.conv_last(x)
-        
+
         return out
+
 
 from torchsummary import summary
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-model = UNet(1,2)
-summary(model, (1,256,256))
-x = torch.ones([5,1,256,256])
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+model = UNet(1, 2)
+summary(model, (1, 256, 256))
+x = torch.ones([5, 1, 256, 256])
 y = model.forward(x)
 
 print(y.shape)
