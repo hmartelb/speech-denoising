e8ca6623fd0464e4af924647d5663047d1a09c05
Héctor Martel <hmartelb@hotmail.com>
2021-06-06 10:03:31 +0100

    get_dataset method (work in progress)

--- data/__init__.py
+++ data/__init__.py
@@ -2,3 +2,21 @@
 # from .UrbanSound8K import UrbanSound8KDataset
 from .NoiseMixer import NoiseMixerDataset
 from .AudioDirectory import AudioDirectoryDataset
+
+import os
+
+LIBRISPEECH_PATH = os.path.join('..', 'datasets', 'LibriSpeech_16kHz_4s')
+URBANSOUND8K_PATH = os.path.join('..', 'datasets', 'UrbanSound8K_16kHz_4s')
+
+def get_dataset(name):
+    if name == 'LibriSpeech':
+        train_data = AudioDirectoryDataset(root=LIBRISPEECH_PATH)
+        val_data = AudioDirectoryDataset(root=LIBRISPEECH_PATH)
+        test_data = AudioDirectoryDataset(root=LIBRISPEECH_PATH)
+        
+    if name == 'UrbanSound8K':
+        train_data = AudioDirectoryDataset(root=URBANSOUND8K_PATH)
+        val_data = AudioDirectoryDataset(root=URBANSOUND8K_PATH)
+        test_data = AudioDirectoryDataset(root=URBANSOUND8K_PATH)
+
+    return train_data, val_data, test_data
